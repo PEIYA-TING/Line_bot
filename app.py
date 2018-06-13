7537a1e647aea27b1df7f40b3647cbcb7e35b3eb
adam100830 <adam100830@gmail.com>
2018-06-13 16:57:09 +0800

    Init

--- app.py
+++ app.py
@@ -68,7 +68,9 @@ def handle_message(event):
     ####
     context = ssl._create_unverified_context()
 
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
+    # headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
+    headers = {"User-Agent": "Mozilla/5.0"}
+
 
     request = urllib.request.Request(url, headers = headers)
 
