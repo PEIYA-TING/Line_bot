7bb6fbc88326713c0e9dd286f339b04cc6eef8b3
adam100830 <adam100830@gmail.com>
2018-06-13 16:27:21 +0800

    Init

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from lxml import html, etree
 import os
 import re
 import pandas as pd
+from urllib.parse import quote
+import urllib.request
+import ssl
 
 from flask import Flask, request, abort
 
@@ -42,18 +45,6 @@ def callback():
 
     return 'OK'
 
-# ##
-# arg_ss = "桃園"
-# arg_checkin_year = 2018
-# arg_checkin_month = 6
-# arg_checkin_monthday= 14
-# arg_checkout_year = 2018
-# arg_checkout_month = 6
-# arg_ckeckout_monthday = 20
-# arg_adults = 2
-# arg_group_children = 0
-# url = 'https://www.booking.com/searchresults.zh-tw.html?ss=\"' + str(arg_ss) + '\"&checkin_year=' + str(arg_checkin_year) + '&checkin_month=' + str(arg_checkin_month) + '&checkin_monthday=' + str(arg_checkin_monthday) + '&checkout_year=' + str(arg_checkout_year) +  '&checkout_month=' + str(arg_checkout_month) + '&ckeckout_monthday=' + str(arg_ckeckout_monthday) + '&group_adults=' + str(arg_adults) + '&group_children=' + str(arg_group_children)
-# ##
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -71,55 +62,87 @@ def handle_message(event):
     arg_ckeckout_monthday = input_str.split()[6]
     arg_adults = input_str.split()[7]
     arg_group_children = input_str.split()[8]
-    url = 'https://www.booking.com/searchresults.zh-tw.html?ss=\"' + str(arg_ss) + '\"&checkin_year=' + str(arg_checkin_year) + '&checkin_month=' + str(arg_checkin_month) + '&checkin_monthday=' + str(arg_checkin_monthday) + '&checkout_year=' + str(arg_checkout_year) +  '&checkout_month=' + str(arg_checkout_month) + '&ckeckout_monthday=' + str(arg_ckeckout_monthday) + '&group_adults=' + str(arg_adults) + '&group_children=' + str(arg_group_children)
+    url = 'https://www.booking.com/searchresults.zh-tw.html?ss=\"' + quote(str(arg_ss)) + '\"&checkin_year=' + quote(str(arg_checkin_year)) + '&checkin_month=' + quote(str(arg_checkin_month)) + '&checkin_monthday=' + quote(str(arg_checkin_monthday)) + '&checkout_year=' + quote(str(arg_checkout_year)) +  '&checkout_month=' + quote(str(arg_checkout_month)) + '&ckeckout_monthday=' + quote(str(arg_ckeckout_monthday)) + '&group_adults=' + quote(str(arg_adults)) + '&group_children=' + quote(str(arg_group_children))
     ##
 
     ####
-    result = requests.get(url)  
-    result.encoding = 'utf8'
-    root = etree.fromstring(result.content, etree.HTMLParser())  
-    # print(root)
-    mode = re.compile(r'\d+\.?\d*')
+    context = ssl._create_unverified_context()
 
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, 'lxml')
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
 
+    request = urllib.request.Request(url, headers = headers)
+
+    response = urllib.request.urlopen(request, context = context)
+
+    soup = BeautifulSoup(response, 'html.parser')
+
+    ##
+    ### Hotel Title ###
     name = soup.select('.sr-hotel__name')
     name_list = []
 
     for i in range(len(name)):
         a = name[i].contents
         name_list.append(a[0].split("\n")[1])
+
+    ### Hotel Price ###
+    #price availprice no_rack_rate
+    price_list = soup.find_all("strong", {"class":"price"})
+    prices_list = []
+
+    for price in price_list:  
+    #     print(price.text.replace(u'\xa0', u' ').replace("\n", "").split()[1])
+        p = price.text.replace(u'\xa0', u' ').replace("\n", "").split()[1]
         
-    
+        if len(p) > 3:
+            c = p.split(',')
+            Price = c[0] + c[1]
+            Price = int(Price)
+        
+        else:
+            Price = int(p)
+            
+        prices_list.append(Price)
 
-    link = root.xpath("//a[@class='hotel_name_link url']")
-    # print(soup.select('.hotel_name_link'))
-    u_total = []
-    u1 = "https://www.booking.com/"
+    ### Hotel Score ###
+    star = soup.find_all("span",{"class":"review-score-badge"})
+    score_list = []
+
+
+    for i in range(len(star)):
+    #     print(star[i].text.split()[0])
+        score_list.append(star[i].text.split()[0])
+
+    ### Hotel URL ###
+    link = soup.find_all("a",{"class":"hotel_name_link url"})
+    url1 = "https://www.booking.com/"
+    url_list = []
+
     for i in range(len(link)):
-        u2 = link[i].attrib['href'].split("\n")[1] #https://www.booking.com/hotel/tw/beginning-hostel.zh-tw.html#hotelTmpl
-        u3 = link[i].attrib['href'].split("\n")[2]
-        u_total.append(u1 + u2 + u3)
+        url2 = link[i].get('href').split("\n")[1]
+        url3 = link[i].get('href').split("\n")[2]
+        url = url1 + url2 + url3
+        url_list.append(url)
 
+    ### Hotel Image URL ###
+    img_url = soup.find_all("img",{"class":"hotel_image"})
+    img_list = []
 
-    num = root.xpath("//span[@class='review-score-badge']")
-    score = []
+    for i in range(len(img_url)):
+        img_list.append(img_url[i].get('src'))
 
-    for i in range(len(num)):
-        number = mode.findall(num[i].text)[0]
-        score.append(number)
+    length = min(len(name_list),len(prices_list),len(star),len(link),len(img_url))
 
+    end = length - 2
+    name_list = name_list[0:end]
+    prices_list = prices_list[0:end]
+    url_list = url_list[0:end]
+    score_list = score_list[0:end]
+    img_list = img_list[0:end]
 
-    pic = root.xpath("//img[@class='hotel_image']")
-    pic_url = []
+    hotel_df = pd.DataFrame({"Hotel_name":name_list,"Hotel_price":prices_list,"Hotel_url":url_list,"Hotel_score":score_list,"Hotel_pic":img_list})
+    hotel_df = hotel_df.sort_values(by = ['Hotel_score','Hotel_price'],ascending=False).reset_index(drop=True)
 
-    for i in range(len(pic)):
-        pic_url.append(pic[i].attrib['src'])
-
-    hotel_df = pd.DataFrame({"Hotel_name":name_list,"Hotel_url":u_total,"Hotel_score":score,"Hotel_pic":pic_url})
-    hotel_df = hotel_df.sort_values(by = ['Hotel_score'],ascending=False).reset_index(drop=True)
-    
     output = ""
     for i in range(6):
         tmp = hotel_df.iloc[i,0] + " " + hotel_df.iloc[i,1] + " " + hotel_df.iloc[i,2]
@@ -127,13 +150,6 @@ def handle_message(event):
         output = output + "\n"
         tmp = ""
 
-    # output =hotel_df.iloc[1,0] + " " + hotel_df.iloc[1,1] + " " + hotel_df.iloc[1,2]
-    # output = ""
-    # for i in range(len(name_list)):
-    #     tmp = name_list[i] + " " + u_total[i] + " " + score[i]
-    #     output = output + tmp 
-    #     output = output + "\n"
-    #     tmp = ""
 
     ####
     
