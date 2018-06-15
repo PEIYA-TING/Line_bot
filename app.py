c60cb4c50ed4a717ff60e278ae7ee04b683a7818
adam100830 <adam100830@gmail.com>
2018-06-15 10:22:45 +0800

    Init

--- app.py
+++ app.py
@@ -170,29 +170,29 @@ def handle_message(event):
                         uri=hotel_df.iloc[0,1]
                     )
                 ]
-            ),
-            CarouselColumn(
-                thumbnail_image_url=hotel_df.iloc[1,3],
-                title = hotel_df.iloc[1,0],
-                text = 'hotel 2',
-                actions=[
-                    URITemplateAction(
-                        label='旅館為：'+str(hotel_df.iloc[1,0])+'，評分為：'+str(hotel_df.iloc[1,2]),
-                        uri=hotel_df.iloc[1,1]
-                    )
-                ]
-            ),
-            CarouselColumn(
-                thumbnail_image_url=hotel_df.iloc[2,3],
-                title = hotel_df.iloc[2,0],
-                text = 'hotel 3',
-                actions=[
-                    URITemplateAction(
-                        label='旅館為：'+str(hotel_df.iloc[2,0])+'，評分為：'+str(hotel_df.iloc[2,2]),
-                        uri=hotel_df.iloc[2,1]
-                    )
-                ]
             )
+            # CarouselColumn(
+            #     thumbnail_image_url=hotel_df.iloc[1,3],
+            #     title = hotel_df.iloc[1,0],
+            #     text = 'hotel 2',
+            #     actions=[
+            #         URITemplateAction(
+            #             label='旅館為：'+str(hotel_df.iloc[1,0])+'，評分為：'+str(hotel_df.iloc[1,2]),
+            #             uri=hotel_df.iloc[1,1]
+            #         )
+            #     ]
+            # ),
+            # CarouselColumn(
+            #     thumbnail_image_url=hotel_df.iloc[2,3],
+            #     title = hotel_df.iloc[2,0],
+            #     text = 'hotel 3',
+            #     actions=[
+            #         URITemplateAction(
+            #             label='旅館為：'+str(hotel_df.iloc[2,0])+'，評分為：'+str(hotel_df.iloc[2,2]),
+            #             uri=hotel_df.iloc[2,1]
+            #         )
+            #     ]
+            # )
         ]     
     )
 
